bbebb815710500afdfbee2da6a23dd68b278fe7b
unknown <eitannaim@gmail.com>
2019-07-03 14:20:11 +0300

    372019

--- Ctrl.js
+++ Ctrl.js
@@ -1,4 +1,13 @@
 app.controller("GamblerCtrl", function ($scope,$http){
-
+$scope.a;
+function myFunction() {
+    //  document.body.style.backgroundColor = "yellow";
+      //document.body.style.backgroundImage = "url('roll1.jpg')";
+      a=document.getElementById("demo").innerHTML =Math.floor((Math.random())* Math.floor(38));
+      console.log(a);
+     
+     // to check
+     // document.body.loader.backgroundcolor="red";
+    }
 }
 );
