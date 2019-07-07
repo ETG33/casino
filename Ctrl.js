fef75688209a90960c24578d2e35533a94a2204e
unknown <eitannaim@gmail.com>
2019-07-07 11:30:42 +0300

    sunday7719

--- Ctrl.js
+++ Ctrl.js
@@ -1,5 +1,23 @@
 app.controller("GamblerCtrl", function ($scope,$http){
-$scope.a=document.getElementById("demo").innerHTML =Math.floor((Math.random())* Math.floor(38));;
+;
 
+$scope.count = 0;
+//$scope.a=document.getElementById("demo").innerHTML =Math.floor((Math.random())* Math.floor(38));
+var a;
+/*function myFunction() {
+    //  document.body.style.backgroundColor = "yellow";
+      //document.body.style.backgroundImage = "url('roll1.jpg')";
+      a=document.getElementById("demo").innerHTML =Math.floor((Math.random())* Math.floor(38));
+      console.log(a);
+      alert(1);
+      if (a%2==0){
+        document.body.style.backgroundColor = "red";
+      }
+     else{
+      document.body.style.backgroundColor = "black";
+     }
+     // to check
+     // document.body.loader.backgroundcolor="red";
+    }*/  
 }
 );
